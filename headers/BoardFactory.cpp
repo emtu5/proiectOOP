6055efe59b1002ca850eee57ed884d7f73018fb1
Paul <paulaesthetical@gmail.com>
2023-05-27 21:28:29 +0300

    changed "noOrtho" to "noAdjacent" for game files

--- headers/BoardFactory.cpp
+++ headers/BoardFactory.cpp
@@ -13,7 +13,7 @@ std::shared_ptr<Board>
 BoardFactory::createBoard(const std::string &type, const std::vector<std::string> &boardLayout, int size, const sf::Vector2i &pos, int no_pieces) {
     if (type == "fill") { return std::make_shared<FilledBoard>(boardLayout, size, 'w', pos);}
     else if (type == "fill-1") { return std::make_shared<FilledBut1Board>(boardLayout, size, 'x', pos);}
-    else if (type == "noOrtho") { return std::make_shared<NoAdjacentBoard>(boardLayout, size, 'y', pos, no_pieces);}
+    else if (type == "noAdjacent") { return std::make_shared<NoAdjacentBoard>(boardLayout, size, 'y', pos, no_pieces);}
     else if (type == "no2x2") { return std::make_shared<No2x2Board>(boardLayout, size, 'z', pos, no_pieces);}
     return nullptr;
 }
