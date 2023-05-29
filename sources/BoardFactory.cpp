7db4c54d5a5c3cc145455334af827dd223727be8
Paul <paulaesthetical@gmail.com>
2023-05-29 14:13:11 +0300

    Changed board texture order

--- sources/BoardFactory.cpp
+++ sources/BoardFactory.cpp
@@ -11,10 +11,10 @@ BoardFactory &BoardFactory::get_factory() {
 
 std::shared_ptr<Board>
 BoardFactory::createBoard(const std::string &type, const std::vector<std::string> &boardLayout, int size, int w, int h, const sf::Vector2i &pos, int no_pieces) {
-    if (type == "fill") { return std::make_shared<FilledBoard>(boardLayout, size, 'w', w, h, pos);}
-    else if (type == "fill-1") { return std::make_shared<FilledBut1Board>(boardLayout, size, 'x', w, h, pos);}
-    else if (type == "noAdjacent") { return std::make_shared<NoAdjacentBoard>(boardLayout, size, 'y', w, h, pos, no_pieces);}
-    else if (type == "no2x2") { return std::make_shared<No2x2Board>(boardLayout, size, 'z', w, h, pos, no_pieces);}
-    else if (type == "allTouch") { return std::make_shared<AllTouchingBoard>(boardLayout, size, 'z', w, h, pos, no_pieces);}
+    if (type == "fill") { return std::make_shared<FilledBoard>(boardLayout, size, 'z', w, h, pos);}
+    else if (type == "fill-1") { return std::make_shared<FilledBut1Board>(boardLayout, size, 'y', w, h, pos);}
+    else if (type == "noAdjacent") { return std::make_shared<NoAdjacentBoard>(boardLayout, size, 'x', w, h, pos, no_pieces);}
+    else if (type == "no2x2") { return std::make_shared<No2x2Board>(boardLayout, size, 'w', w, h, pos, no_pieces);}
+    else if (type == "allTouch") { return std::make_shared<AllTouchingBoard>(boardLayout, size, 'v', w, h, pos, no_pieces);}
     return nullptr;
 }
