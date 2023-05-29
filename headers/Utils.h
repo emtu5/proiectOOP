1a60cee22a2556d7b39c09d7f97e519ba6be0367
Paul <paulaesthetical@gmail.com>
2023-05-29 12:53:24 +0300

    left a semicolon lol

--- headers/Utils.h
+++ headers/Utils.h
@@ -12,7 +12,7 @@ namespace Utils {
     void drawLevelMessage(sf::RenderWindow &window, sf::Font &, int levelNumber, const std::shared_ptr<Board> &);
     void drawLevelComplete(sf::RenderWindow &window, sf::Font &);
     sf::Text getText(sf::Font &, const std::string &, int, const sf::Vector2f &, const sf::Color &);
-};
+}
 
 
 #endif //OOP_UTILS_H
